eae7f23a008bcbcf6092b9c5e0434dc2186aac7b
Gabriel Zapodeanu <gzapodea@cisco.com>
2019-06-12 17:21:44 -0700

    updated for CL San Diego 2019

---
+++ config.py
@@ -0,0 +1,47 @@
+
+
+# developed by Gabi Zapodeanu, TME, Enterprise Networking, Cisco Systems
+
+
+# This file contains the Webex Auth, ServiceNow Dev account info, router info
+
+WEBEX_TEAMS_URL = 'https://api.ciscospark.com/v1'
+WEBEX_TEAMS_AUTH = 'Bearer ' + 'Paste your token here'
+WEBEX_TEAMS_ROOM = 'ERNA'
+WEBEX_TEAMS_MEMBER = 'email address'
+
+
+SW_HOST = 'ip_address'
+RO_HOST = 'ip_address'
+PORT = 830
+USER ='cisco'
+PASS ='cisco'
+
+
+SNOW_URL = 'https://devxyzxy.service-now.com/api/now'
+SNOW_ADMIN = 'api_user'
+SNOW_DEV = 'dev_user'
+SNOW_PASS = 'password'
+SNOW_INSTANCE = 'devxyzxy'
+
+
+GOOGLE_API_KEY = 'api_key'
+
+
+DNAC_URL = 'ip_address'
+DNAC_USER = 'user'
+DNAC_PASS = 'password'
+
+
+ASAv_URL = 'https://ip_address'
+ASAv_USER = 'api_user'
+ASAv_PASSW = 'password'
+
+
+UCSD_URL = 'https://ip_address'
+UCSD_USER = 'user'
+UCSD_PASSW = 'password'
+UCSD_KEY = 'api_key'
+
+
+APPROVER_EMAIL = 'email_address'
